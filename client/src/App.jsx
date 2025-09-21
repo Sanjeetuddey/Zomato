b1fe051d5856c123ea893a71fcd0dff6c48f024e
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-09-21 16:05:52 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,24 @@
-
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import Header from "./pages/Header";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
-
-import {Toaster} from "react-hot-toast";
+import Dashboard from "./components/Dashboard";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Router>
         <Header />
-         <Toaster/>
+        <Toaster />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </>
