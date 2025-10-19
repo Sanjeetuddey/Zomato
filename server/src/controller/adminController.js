fbe1809a5c854fe3afbdcf451f1b9172485923f5
Sanjeet Uddey <sanjeetuddey@gmail.com>
2025-10-19 23:49:30 +0530

    day24

--- server/src/controller/adminController.js
+++ server/src/controller/adminController.js
@@ -197,6 +197,8 @@ export const AddResturant = async (req, res, next) => {
   }
 };
 
+
+// Get All Resturants
 export const GetAllResturants = async (req, res, next) => {
   try {
     const resturants = await Resturant.find().sort({ createdAt: -1 });
